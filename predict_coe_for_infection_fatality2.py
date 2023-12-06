d0be17960d241258e9d9b12913f30653369fe72c
DerekLuj <152663913+DerekLuj@users.noreply.github.com>
2023-12-05 21:43:11 -0800

    Update predict_coe_for_infection_fatality2.py

--- predict_coe_for_infection_fatality2.py
+++ predict_coe_for_infection_fatality2.py
@@ -1,38 +1,34 @@
-import numpy as np
-from scipy.optimize import curve_fit
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
-data = pd.read_csv('D:/桌面/AMath383/final/ddd3.csv')  # Replace with your CSV file path
-H_values = data['infection_fatality'].values
+data = pd.read_csv('D:/桌面/AMath383/final/ddd3.csv')
+data2 = pd.read_csv('D:/桌面/AMath383/final/ddd2.csv')
 
+# Extracting only the first 60 data points
+H_values = data['infection_fatality'].values[:60]
+H_original = data2['infection_fatality'].values[25:85]  # Adjusted to match the range in data
 
-X = H_values[:-1]
-y = H_values[1:]
+# Define the model function with the optimized parameters
+def model_function(H_k, a, b, c, d, e):
+    return a * H_k + b * H_k**2 + c * H_k**3 + d * H_k**4 + e
 
-def model_function(H_k, a, b,c,d,e):
-    return a * H_k + b * H_k**2 + c*H_k**3 + d*H_k**4 + e
+# Fit the model to get the parameters (using the first 60 points)
+params, _ = curve_fit(model_function, H_values[:-1], H_values[1:])
+a_opt, b_opt, c_opt, d_opt, e_opt = params
 
-params, params_covariance = curve_fit(model_function, X, y)
+# Generate predictions for the first 60 points
+H_predictions = [model_function(H_k, a_opt, b_opt, c_opt, d_opt, e_opt) for H_k in H_values]
 
-a_opt, b_opt, c_opt,d_opt,e_opt= params
-
-# Using the optimized coefficients to predict the infection fatality values
-predicted_values = model_function(H_values[:-1], a_opt, b_opt, c_opt, d_opt, e_opt)
-
-# Preparing dates for plotting (excluding the last date as the prediction is one step ahead)
-data['sequential_number'] = range(1, len(data) + 1)
-sequential_number = data['sequential_number']
-plot_dates = sequential_number[:-1]
-
-# Plotting both original and predicted data
+# Plot the original and predicted infection fatality rates for the first 60 points
 plt.figure(figsize=(12, 6))
-plt.scatter(plot_dates, H_values[:-1], label='Original Data', color='blue')
-plt.plot(plot_dates, predicted_values, label='Predicted Data', color='green', linestyle='dashed', linewidth=2)
-plt.title('Original vs Predicted Infection Fatality Rate')
-plt.xlabel('Date')
+plt.scatter(range(1, len(H_original)+1), H_original, label='Original Data', color='blue')
+plt.plot(range(1, 61), H_predictions, label='Model Predictions', color='red', linewidth=2)
+plt.title('Infection Fatality Rate Predictions for First 60 Data Points')
+plt.xlabel('Data Point Number')
 plt.ylabel('Infection Fatality Rate')
 plt.legend()
 plt.grid(True)
 plt.show()
+
 print(params)
